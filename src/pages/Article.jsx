a943113c880d41a99b218a2d2965192989260a49
Alvin Miftah <evil.devmail@gmail.com>
2023-09-18 16:52:06 +0700

    rewrite class to function

--- src/pages/Article.jsx
+++ src/pages/Article.jsx
@@ -1,94 +1,81 @@
-import { Component } from "react";
+import { Component, useEffect, useState } from "react";
 import { articles } from "../api/data";
 import Card from "../components/Card";
 import './Article.module.css'
 import NotFound from '../assets/transistor-404.png'
 import Input from "../components/Input";
 
-class Article extends Component {
-    constructor(props){
-        super(props)
+const Article = () => {
+    const [data, setData] = useState([]) // state untuk data awal
+    const [filteredData, setFilteredData] = useState([]) // state untuk data yang terfilter
+    const [search, setSearch] = useState('')
 
-        // Initialize state
-        this.state = {
-            data: [],
-            filteredData: [],
-            search: "" // state untuk search input
-        }
-    }
+    useEffect(() => {
+        console.log('calling useEffect()');
+        setData(articles)
+        window.localStorage.setItem('article', JSON.stringify(articles)) // buat menyimpan data ke local storage
+        
+    }, []) 
 
-    componentDidMount(){
-        // logic untuk menambahkan data ke dalam state
-        this.setState({
-            data: articles // isi datanya
-        })
-    }
+    useEffect(() => {
+        const getArticle =  window.localStorage.getItem('article')
+        console.log(JSON.parse(getArticle));
+    })
 
-    onSearch(e){
-        console.log(e.target.value) // mengambil value dari syntathic event
-        this.setState({
-            search: e.target.value
-        },
-        () => this.onFilter(this.state.search))
-    }
-
-    onFilter(query) {
-        let { data } = this.state;
-
+    const onFilter = (query) => {
         // untuk melakukan filtering dari title 
-        let filteredData = data?.filter((item) =>
+        let filter = data?.filter((item) =>
             item.title.toLowerCase().includes(query.toLowerCase())
         );
 
         // update state filteredData untuk mengubah list article
-        this.setState({ filteredData });
+        setFilteredData(filter)
     }
-    
-    // render UI
-    render(){
-        let { data, filteredData , search} = this.state
 
-        return(
-            <>
+    const onSearch = (e) => {
+        setSearch(e.target.value) // mengubah state search
+        onFilter(search)
+    }
+
+    return(
+        <>
             <h1>Berita Hari Ini</h1>
-                <Input value={search} onChange={(e) => this.onSearch(e)} />
-
-                {
-                    search !== "" ? // Jika search query kosong atau tidak sedang melakukan pencarian di kolom search input
-                        <section>
-                            {
-                                filteredData.length > 0 ? filteredData.map((item, index) => (
-                                    <Card 
-                                        key={index}
-                                        author={item.author}
-                                        title={item.title}
-                                        description={item.description}
-                                        urlImage={item.urlToImage}
-                                        date={item.publishedAt}
-                                    />
-                                )) : <img src={NotFound} />
-                            }
-                        </section> 
-                        : // Jika search query tidak kosong atau sedang melakukan pencarian di kolom search input
-                        <section> 
-                            {
-                                data.length > 0 ? data.map((item, index) => (
-                                    <Card 
-                                        key={index}
-                                        author={item.author}
-                                        title={item.title}
-                                        description={item.description}
-                                        urlImage={item.urlToImage}
-                                        date={item.publishedAt}
-                                    />
-                                )) : <img src={NotFound} />
-                            }
-                        </section>
-                }
-            </>
+            <Input value={search} onChange={(e) => onSearch(e)} />
             
-        )
-    }
+            {
+                search !== "" ? // Jika search query kosong atau tidak sedang melakukan pencarian di kolom search input
+                    <section>
+                        {
+                            filteredData.length > 0 ? filteredData.map((item, index) => (
+                                <Card 
+                                    key={index}
+                                    author={item.author}
+                                    title={item.title}
+                                    description={item.description}
+                                    urlImage={item.urlToImage}
+                                    date={item.publishedAt}
+                                />
+                            )) : <img src={NotFound} />
+                        }
+                    </section> 
+                    : // Jika search query tidak kosong atau sedang melakukan pencarian di kolom search input
+                    <section> 
+                        {
+                            data.length > 0 ? data.map((item, index) => (
+                                <Card 
+                                    key={index}
+                                    author={item.author}
+                                    title={item.title}
+                                    description={item.description}
+                                    urlImage={item.urlToImage}
+                                    date={item.publishedAt}
+                                />
+                            )) : <img src={NotFound} />
+                        }
+                    </section>
+            }
+        </>
+    )
 }
 
 export default Article
